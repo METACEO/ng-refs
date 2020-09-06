1470018e2e4b79c55194f7138431d758bd972c2c
James Allen <james.allen@meta.ceo>
2020-09-06 15:54:09 -0500

    feat: copy README material during build

--- build.sh
+++ build.sh
@@ -11,3 +11,8 @@ yarn affected:e2e --headless
 
 echo Build our library
 yarn build ng-refs --prod
+
+echo Copy over README material
+cp ./README.md ./dist/libs/ng-refs
+cp ./README-after.png ./dist/libs/ng-refs
+cp ./README-before.png ./dist/libs/ng-refs
